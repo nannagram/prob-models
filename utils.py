8dcdfba535c66bd2d5db81cb92353f610694cc8d
Francesca <francesca.santucci@imtlucca.it>
2024-12-03 15:02:26 +0100

    added data import

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import numpy as np
 
 def read_add_paths(path):
     with open(path, 'r') as f:
@@ -11,3 +13,9 @@ def read_paths(path):
     with open(path, 'r') as f:
         paths = f.read().splitlines()
     return paths
+
+def standardise(sub):
+    return (sub - np.mean(sub,axis=0)) / np.std(sub,axis=0)
+
+def getdata(path):
+    return {file.split('_')[1].split('-')[1]: standardise(np.loadtxt(path+file)) for i,file in enumerate(os.listdir(path)) if file.split('_')[0]=='ts' }
