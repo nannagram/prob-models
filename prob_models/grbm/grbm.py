4b0071dcb7406731f0de5054b56a165d26fa287c
stansstanman <ruggero.basanisi@gmail.com>
2024-12-04 16:37:47 +0100

    defined function to run grbm

--- prob_models/grbm/grbm.py
+++ prob_models/grbm/grbm.py
@@ -1,21 +1,16 @@
 import numpy as np
 from numpy.random import default_rng
-import pydeep
 from pydeep.rbm.model import GaussianBinaryRBM
 import os
 import os.path as op
-import importlib
+import pandas as pd
 from prob_models.utils.utils import read_paths, getdata
 from pydeep.rbm.trainer import CD
 from pydeep.rbm.estimator import (partition_function_factorize_h, 
                                   log_likelihood_v, 
-                                  reconstruction_error)
-from joblib import Parallel, delayed
+                                  reconstruction_error,
+                                  annealed_importance_sampling)
 
-
-# def n_hid_parallel(data, n_hid=np.arange(2, 101), n_jobs=2):
-#     Parallel(n_jobs=n_jobs)(delayed(n_hid_single)(data, n_hid) for n_hid in n_hid)
-#     returnimport numpy as np
 
 def split_train_test(X, train_fraction, standardize=False, shuffle=True, seed=None):
     rng = default_rng(seed)
@@ -38,36 +33,54 @@ def split_train_test(X, train_fraction, standardize=False, shuffle=True, seed=No
     return Xtrain, Xtest
 
 
-# Load data 
-paths = read_paths(op.join(os.getcwd(), op.join('..', '..', 'paths.txt')))
-datadic = getdata(paths,dataset='CamCAN')
-T, N = np.shape(list(datadic.values())[0])
+def train_rbm(X_train, X_test, n_hid, epochs):
+    n_v = X1_train.shape[-1]
+    variances = np.var(X1_train,axis=0,keepdims=True)
+    
+    results = {}
+    for n_h in n_hid:
+        print(f'\nTraining with {n_h} hidden units')
+        
+        grbm = GaussianBinaryRBM(number_visibles=n_v,
+                                number_hiddens=n_h,
+                                data=X1_train,initial_sigma=variances,
+                                initial_visible_offsets=0.0,
+                                initial_hidden_offsets=0.0)
+        trainer = CD(grbm)
+        
+        for epoch in range(epochs):
+            trainer.train(data=X1_train)
+            
+            if epoch % 10 == 0:
+                print(f'Epoch {epoch}')
+        
+        print('Computing log-likelihood and reconstruction error...')
+        # log_z = partition_function_factorize_h(grbm, status=True)
+        log_z = annealed_importance_sampling(grbm, status=True)
+        ll_train = np.mean(log_likelihood_v(grbm, log_z, X1_train))
+        ll_test = np.mean(log_likelihood_v(grbm, log_z, X1_test))
+        re = np.mean(reconstruction_error(grbm, X1_train))
+        print('...done')
+        
+        results[n_h] = {'ll_train': ll_train, 'll_test': ll_test, 're': re}
+        
+    results = pd.DataFrame.from_dict(results, orient='index')
+    return results
 
-tf = 0.8
-sj_names = [name for name in datadic.keys()]
-X1 = datadic[1]
 
-X1_train, X1_test = split_train_test(X1, train_fraction=tf, standardize=True, seed=0)
+if __name__ == '__main__':
+    # Load data 
+    paths = read_paths(op.join(os.getcwd(), op.join('..', '..', 'paths.txt')))
+    datadic = getdata(paths, dataset='CamCAN')
+    T, N = np.shape(list(datadic.values())[0])
 
-n_v = X1_train.shape[-1]
-n_h = 20
-variances = np.var(X1_train,axis=0,keepdims=True)
-grbm = GaussianBinaryRBM(number_visibles=n_v,number_hiddens=n_h,
-                         data=X1_train,initial_sigma=variances,
-                         initial_visible_offsets=0.0,
-                         initial_hidden_offsets=0.0)
+    tf = 0.8
+    X1 = datadic[1]
 
-trainer = CD(grbm)
+    X1_train, X1_test = split_train_test(X1, train_fraction=tf, standardize=True, seed=0)
+    print(f'X1 shape: {X1_train.shape}')
 
-epochs = 50
-for epoch in range(epochs):
-    trainer.train(data=X1_train)
-
-log_z = partition_function_factorize_h(grbm)
-ll_train = np.mean(log_likelihood_v(grbm, log_z, X1_train))
-ll_test = np.mean(log_likelihood_v(grbm, log_z, X1_test))
-re = np.mean(reconstruction_error(grbm, X1_train))
-
-print(ll_train)
-print(ll_test)
-print(re)
+    n_hid = np.arange(2, 10, 1)
+    epochs = 100
+    results = train_rbm(X1_train, X1_test, n_hid, epochs)
+    print(results)
