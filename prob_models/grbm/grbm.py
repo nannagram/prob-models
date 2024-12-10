86d223ac88564755eda24e901440d4875d657b34
Gabriele Poidomani <gabriele.poidomani@imtlucca.it>
2024-12-10 11:26:42 +0100

    stashing local changes for grbm.py to sync with remote repository

--- prob_models/grbm/grbm.py
+++ prob_models/grbm/grbm.py
@@ -1,5 +1,6 @@
 import numpy as np
 from numpy.random import default_rng
+import random
 from pydeep.rbm.model import GaussianBinaryRBM
 import os
 import os.path as op
@@ -10,7 +11,7 @@ from pydeep.rbm.estimator import (partition_function_factorize_h,
                                   log_likelihood_v, 
                                   reconstruction_error,
                                   annealed_importance_sampling)
-
+from pydeep.rbm.sampler import GibbsSampler
 
 def split_train_test(X, train_fraction, standardize=False, shuffle=True, 
                      seed=None):
@@ -33,7 +34,28 @@ def split_train_test(X, train_fraction, standardize=False, shuffle=True,
 
     return Xtrain, Xtest
 
-
+# Train model and return the model class
+# Find a better name!
+def learn_rbm(X_train, X_test, n_hid, epochs):
+    n_v = X_train.shape[-1]
+    variances = np.var(X_train, axis=0, keepdims=True)
+    
+                
+    grbm = GaussianBinaryRBM(number_visibles=n_v,
+                            number_hiddens=n_hid,
+                            data=X_train,
+                            initial_sigma=variances,
+                            initial_visible_offsets=0.0,
+                            initial_hidden_offsets=0.0)
+    trainer = CD(grbm)
+    
+    for epoch in range(epochs):
+        
+        trainer.train(data=X_train, epsilon=0.1)
+        
+        if epoch % 10 == 0:
+            print(f'Epoch {epoch}')
+    return grbm
 def train_rbm(X_train, X_test, n_hid, epochs):
     n_v = X_train.shape[-1]
     variances = np.var(X_train, axis=0, keepdims=True)
@@ -76,6 +98,16 @@ def single_rbm(X, n_hid, tf=0.8, epochs=100):
     results, paramas = train_rbm(X_train, X_test, n_hid, epochs)
     return results, paramas
 
+# Samples the visible variables from conditional distribution.
+def dreams(v, ndreams,gibbssampler, n_v,k=400):
+    dreams=[]
+    v = np.zeros(n_v)
+    for i in range(ndreams):
+        # v=random.sample(test_data,1)
+        v=gibbssampler.sample(v, k=k, betas=None, ret_states=True)[0]
+        dreams.append(v)
+    return np.array(dreams)
+
 if __name__ == '__main__':
     # Load data 
     paths = read_paths(op.join(os.getcwd(), op.join('..', '..', 'paths.txt')))
@@ -85,21 +117,60 @@ if __name__ == '__main__':
     T, N = np.shape(list(datadic.values())[0])
 
     tf = 0.8
-    # X1 = datadic[1]
-    # X_train, X_test = [], []
+
+    ##########################################################################
+    # Sample x values from P(x;h,params) and build theorical correlation matrix
+    # Compute also the empirical correlation matrix on the train set for comparison
+    X1 = datadic[1]
+    X1_train, X1_test = split_train_test(X1, train_fraction=tf, standardize=True, seed=None)
+    n_v = X1_train.shape[-1]
+    n_h = 8
+    variances = np.var(X1_train, axis=0, keepdims=True)
+    grbm = learn_rbm(X1_train, X1_test, n_hid=n_h, epochs=100)
+
+
+    gibbsampler = GibbsSampler(grbm)
+    ndreams = 1000
+    idx = np.random.choice(X1_train.shape[0])
+    v_start = X1_train[1,:]
+    X_dreams = dreams(v_start, ndreams, gibbsampler, n_v, k=400)
+    E_dreams = np.corrcoef(X_dreams.T)
+    # E_dreams = ndreams**-1 * X_dreams.T @ X_dreams
+    T = X1_train.shape[0]
+    E_empir = np.corrcoef(X1_train.T)
+
+    W = grbm.get_parameters()[0]
+
+    J = W @ W.T
+    # E_empir = T**-1 * X1_train.T @ X1_train
+    # Saving results outside repository
+    save_path = op.dirname(op.dirname(op.dirname(os.getcwd())))
+    print(save_path)
+    np.save(op.join(save_path, 'E_dreams.npy'), E_dreams)
+    np.save(op.join(save_path, 'E_empir.npy'), E_empir)
+    np.save(op.join(save_path, 'J.npy'), J)
+###################################################################################
+    '''Workflow for all subjects'''
+
     # for x in datadic.values():
-    #     _X_train, _X_test = split_train_test(x, train_fraction=tf, 
-    #                                          standardize=True, seed=None)
-    #     X_train.append(_X_train)
-    #     X_test.append(_X_test)
+    #     X_train, X_test = [], []
+    #     for x in datadic.values():
+    #         _X_train, _X_test = split_train_test(x, train_fraction=tf, 
+    #                                             standardize=True, seed=None)
+    #         X_train.append(_X_train)
+    #         X_test.append(_X_test)
+            
+    #     X_train = np.concatenate(X_train, axis=0)
+    #     X_test = np.concatenate(X_test, axis=0)
+
+##################################################################################
+    ''' Workflow for single subject '''
+
+    # for sj, x in datadic.items():
+    #     results, params = single_rbm(x, n_hid=[25], tf=0.8, epochs=100)
         
-    # X_train = np.concatenate(X_train, axis=0)
-    # X_test = np.concatenate(X_test, axis=0)
-    for sj, x in datadic.items():
-        results, params = single_rbm(x, n_hid=[25], tf=0.8, epochs=100)
-        
-        sub_results_dir = op.join(results_dir, f'sub_{sj}')
-        if not op.exists(sub_results_dir):
-            os.makedirs(sub_results_dir)
-        results.to_csv(op.join(sub_results_dir, 'results.csv'))
-        np.save(op.join(sub_results_dir, 'params.npy'), params)
+    #     sub_results_dir = op.join(results_dir, f'sub_{sj}')
+    #     if not op.exists(sub_results_dir):
+    #         os.makedirs(sub_results_dir)
+    #     results.to_csv(op.join(sub_results_dir, 'results.csv'))
+    #     np.save(op.join(sub_results_dir, 'params.npy'), params)
